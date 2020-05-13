9d73d57a57bbda7cefe04f1d3d1a1cb2bc159bee
Emerald Laurente <e.s.laurente@gmail.com>
2020-05-13 07:45:47 -0700

    Added ngrx/effects

--- ui-app/src/app/root-store/root-store.module.ts
+++ ui-app/src/app/root-store/root-store.module.ts
@@ -4,6 +4,7 @@ import { StoreModule } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
         strictActionImmutability: true,
       }
     }),
+    EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreRouterConnectingModule.forRoot(),
   ],
